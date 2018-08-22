99a1705ceb7a0fb9ee6e3586bd8a6fcccc055823
Rafael Minguet <rminguet@gmail.com>
2018-08-22 16:12:28 +0200

    SlideButtons Component

---
+++ src/components/Slides.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <slides-buttons/>
+    </div>
+</template>
+
+<script>
+    import SlidesButtons from './SlidesComponent/SlidesButtons.vue'
+    import SlidesCard from './SlidesComponent/SlidesCard.vue'
+
+    export default {
+        name      : 'Slides',
+        components: {
+            SlidesButtons,
+            SlidesCard
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
