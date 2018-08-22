d391412168bef3a12c572c6943ce3d6ed1cc0c17
Rafael Minguet <rminguet@gmail.com>
2018-08-22 17:37:36 +0200

    Changed images resolution

--- src/App.vue
+++ src/App.vue
@@ -34,4 +34,14 @@
         background-size: cover;
     }
 
+    @media (min-width: 1024px) {
+        body {
+            background: url(./assets/imgs/slide-component/corrugated_texture-big.jpg) no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+    }
+
 </style>
