66132f1704f5426b95bc3367f852ff8946729f18
Rafael Minguet <rminguet@gmail.com>
2018-08-22 16:10:28 +0200

    New structure

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+    @font-face {
+        font-family: KBREINDEERGAMES;
+        src: url(./assets/fonts/KBREINDEERGAMES.ttf);
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    @font-face {
+        font-family: PWSmallIcons;
+        src: url(./assets/fonts/PWSmallIcons.ttf);
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    @font-face {
+        font-family: Tofino;
+        src: url(./assets/fonts/tofino-personal/TofinoPersonal-Book.ttf);
+    }
+
+    #app {
+        font-family: 'Tofino', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    body {
+        background: url(./assets/imgs/corrugated_texture.jpg) no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+
 </style>
