28624c3de7300b00afb02d20ea520e81e06f5835
Rafael Minguet <rminguet@gmail.com>
2018-08-23 12:25:26 +0200

    SlideCard (with static content)

--- src/components/SlideComponent/SlideCard.vue
+++ src/components/SlideComponent/SlideCard.vue
@@ -1,20 +1,44 @@
 <template>
     <div class="absolute flex justify-center items-center h-100 w-100">
-        <div class="br4 bg-white v-mid mb5 mb0-ns ml5-ns w-80 _h-70 _mw-70-xl _mh-65-special-1024-768 _mh-80-special-768-1024">
-
+        <div class="flex flex-wrap justify-center items-center br4 bg-white v-mid mb5 mb0-ns ml5-ns w-80 _h-70 _mw-70-xl _mh-65-special-1024-768 _mh-80-special-768-1024">
+            <div class="br4 _h-35 _h50-landscape w-100 w-40-ns">
+                <slide-card-main/>
+            </div>
+            <div class="br4 ph3 pb3 f7 tc w-100 w-60-ns">
+                <slide-card-content/>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import SlideCardMain from './SlideCardMain.vue'
+    import SlideCardContent from './SlideCardContent.vue'
+
     export default {
-        name: "SlideCard"
+        name      : "SlideCard",
+        components: {
+            SlideCardMain,
+            SlideCardContent
+        }
     }
 </script>
 
 <style scoped>
+
+    ._h-35 {
+        height: 35%;
+    }
+
     ._h-70 {
         height: 70%;
+    }
+
+    @media (orientation: landscape) {
+        ._h50-landscape {
+            height: 50%;
+            max-height: 50%;
+        }
     }
 
     @media (min-width: 1025px) {
---
+++ src/components/SlideComponent/SlideCardContent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <span class="kbreindeergames f4">¡Bienvenidos!</span>
+        <br><br>Hemos hecho esta aplicación para que nos ayudes a organizar nuestra boda. Simplemente queremos que
+        contestes unas preguntas para saber cuántos vendréis, qué música queréis y si hay alguna cosa que considerar. Lo
+        que pretendemos es que todo el mundo esté a gusto y disfrute de la fiesta con nosotros.
+        <br><br>Pulsa la <span class="tofino-icon">j</span> de la esquina superior derecha si quieres ver las
+        instrucciones de la aplicación.
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SlideCardContent"
+    }
+</script>
+
+<style scoped>
+
+    .kbreindeergames {
+        font-family: 'KBREINDEERGAMES';
+    }
+
+    .tofino-icon {
+        font-family: 'PWSmallIcons';
+    }
+
+</style>
