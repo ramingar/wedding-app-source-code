7f6f1795c86e56090ad99b01073f9395bbcf9ce0
Rafael Minguet <rminguet@gmail.com>
2018-08-23 15:06:52 +0200

    Portrait

--- src/components/SlideComponent/SlideCard.vue
+++ src/components/SlideComponent/SlideCard.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="absolute flex justify-center items-center h-100 w-100">
-        <div class="flex flex-wrap justify-center items-center br4 bg-white v-mid mb5 mb0-ns ml5-ns w-80 _h-70 _mw-70-xl _mh-65-special-1024-768 _mh-80-special-768-1024">
-            <div class="br4 _h-35 _h50-landscape w-100 w-40-ns">
+    <div class="absolute flex items-center justify-center h-100 w-100">
+        <div class="bg-white overflow-hidden br4 mh3 mb5 mb0-ns ml5-ns _w5_50 _w6_50-l h-75">
+            <div class="pa3 _h-40">
                 <slide-card-main/>
             </div>
-            <div class="br4 ph3 pb3 f7 tc w-100 w-60-ns">
+            <div class="pa3 f7">
                 <slide-card-content/>
             </div>
         </div>
@@ -25,40 +25,13 @@
 </script>
 
 <style scoped>
-
-    ._h-35 {
-        height: 35%;
+    ._w5_50 {
+        width: 24rem;
     }
 
-    ._h-70 {
-        height: 70%;
+    ._h-40 {
+        height: 40%;
     }
 
-    @media (orientation: landscape) {
-        ._h50-landscape {
-            height: 50%;
-            max-height: 50%;
-        }
-    }
-
-    @media (min-width: 1025px) {
-        ._mw-70-xl {
-            max-width: 70%;
-        }
-    }
-
-    @media (width: 1024px) and (height: 768px) {
-        ._mh-65-special-1024-768 {
-            height: 65%;
-            max-height: 65%;
-        }
-    }
-
-    @media (width: 768px) and (height: 1024px) {
-        ._mh-80-special-768-1024 {
-            height: 80%;
-            max-height: 80%;
-        }
-    }
 
 </style>
